fb9b23fae5716a2b78b1d1a85bfdaa378ecb02b7
Bryamslm <bryamlopez1882002@estudiantec.cr>
2021-03-31 23:26:51 -0600

    Carga de proyecto con inicio de sesión listo

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bryam
+ */
+public class Main {
+    public static void main(String[] args) throws IOException{
+        UsuarioJFrame primerVentana= new UsuarioJFrame();
+        
+       
+        primerVentana.setVisible(true);
+    }
+    
+}
---
+++ src/main/java/Usuarios.java
@@ -0,0 +1,43 @@
+
+import java.io.Serializable;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bryam
+ */
+public class Usuarios implements Serializable{
+    
+    private String nombre;
+    private int edad;
+    private String sexo;
+    private double peso;
+    private double estatura;
+    private String usuario;
+    private String contrasena;
+    
+    Usuarios(String nombre, int edad, String sexo, double peso, double estatura, String usuario, String contrasena){
+        
+        this.nombre=nombre;
+        this.edad=edad;
+        this.sexo=sexo;
+        this.peso=peso;
+        this.estatura=estatura;
+        this.usuario=usuario;
+        this.contrasena=contrasena;
+        
+    }
+    public String getUsuario(){
+        return this.usuario;
+    }
+    public String getContra(){
+        return this.contrasena;
+    }
+    
+    
+}
